bc5dcde49e89e075435b85499a584501400c84db
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2025-07-10 09:51:03 -0300

    Integrate Sentry for error tracking and update project dependencies
    - Added Sentry configuration to the Next.js application for enhanced error tracking and monitoring.
    - Updated package.json to include the latest version of @sentry/nextjs and other dependencies.
    - Refactored postcss.config.mjs to utilize autoprefixer alongside tailwindcss.
    - Revised globals.css to implement Tailwind CSS's utility classes and removed outdated styles.
    - Updated .gitignore to exclude the Sentry configuration file.

---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a232bf4b961c22a1ce3c5c038802d455@o4509494124806144.ingest.us.sentry.io/4509644276891648",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
